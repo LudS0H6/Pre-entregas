c82588da1788334a41fafb1f44733a0899de8164
Ludmila <lusapiheffel@gmail.com>
2026-06-27 16:00:18 -0300

    Pre entrega 2

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,19 @@
 const nombre_usuario = prompt("Ingrese su nombre") ; 
 const apellido_usuario = prompt("Ingrese su apellido") ;
-const nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
-const año = 2026 ;
-    
-    console.log(nacimiento_usuario) ;
-    console.log(typeof nacimiento_usuario) ;
+let nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
+const anio = 2026 ;
 
-const edad = año - nacimiento_usuario ;
-    console.log (edad);
-
-alert("Bienvenido " + nombre_usuario + " " + apellido_usuario + ". Usted tiene " + edad + " años.");
-
-
+let edad = anio - nacimiento_usuario ;
+    while (edad < 18){
+       alert("Debes ser mayor de edad para ingresar a este sitio.");
+            nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
+            edad = anio - nacimiento_usuario ;
+                if (edad < 0){
+                    alert("Por favor ingrese un año válido.") ;
+                    nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
+                    edad = anio - nacimiento_usuario ;   
+                    }
+    }
+    if (edad >= 18){
+        alert("Bienvenido " + nombre_usuario + " " + apellido_usuario + ". Usted tiene " + edad + " años.");
+    }
