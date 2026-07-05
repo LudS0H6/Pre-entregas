f8582e39c463d7c132daf749cd78f85f4bd37e31
Ludmila <lusapiheffel@gmail.com>
2026-07-04 21:13:03 -0300

    Entrega 3

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,85 @@
-const nombre_usuario = prompt("Ingrese su nombre") ; 
-const apellido_usuario = prompt("Ingrese su apellido") ;
-let nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
-const anio = 2026 ;
-
-let edad = anio - nacimiento_usuario ;
-    while (edad < 18){
-       alert("Debes ser mayor de edad para ingresar a este sitio.");
-            nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
-            edad = anio - nacimiento_usuario ;
-                if (edad < 0){
-                    alert("Por favor ingrese un año válido.") ;
-                    nacimiento_usuario = parseInt(prompt("Ingrese su año de nacimiento")) ;
-                    edad = anio - nacimiento_usuario ;   
-                    }
+const user = "Maguito" ;
+const password = "1234" ;
+solicitarDatos();
+function solicitarDatos(){
+    let usuario = prompt("Ingresar su usuario");
+    let pass = prompt("Ingresar su contraseña");
+    if (usuario === user && pass === password){ 
+     iniciarCarrito();
+    }else{
+        alert ("Datos incorrectos");
     }
-    if (edad >= 18){
-        alert("Bienvenido " + nombre_usuario + " " + apellido_usuario + ". Usted tiene " + edad + " años.");
+}
+function iniciarCarrito(){
+  let lista = " " ; 
+  let finalizarCarrito = false ; 
+  let total = 0
+  while (!finalizarCarrito){ 
+    let nombreProducto = prompt("Ingrese el producto que desea comprar");
+    let producto = obtenerProducto (nombreProducto); 
+    let precio = obtenerPrecio (nombreProducto) 
+    
+    if(producto){
+      lista += "\n -" + producto ; 
+      total += precio ;
+    }else{
+      if (nombreProducto === null){
+        finalizarCarrito = true ; 
+      } else {
+        alert("Ingrese un producto válido");
+      }
     }
+  }
+let dinero = 20 
+    if(lista != " "){ 
+    let respuesta = confirm("Desea concretar la compra de " +lista + "\n"+ "El total es $" +total); 
+    if(respuesta){
+        if(total < dinero){
+        alert("Usted compró " +lista);
+        }else if(total > dinero){
+        alert("No tiene dinero suficiente.")
+  }
+    }
+  } 
+}
+  function obtenerProducto (cod){
+    let producto ; 
+    switch (cod){
+      case "1":
+        producto = "Libro mágico";
+        break;
+      case "2":
+        producto = "Poción";
+        break;
+      case "3":
+        producto = "Polvo de hadas";
+        break;
+      case "4":
+        producto = "Pluma de Fenix";
+        break;
+      default:
+        producto = false; 
+    }
+    return producto
+  } 
+  function obtenerPrecio (cod){
+    let precio ; 
+    switch (cod){
+      case "1":
+        precio = 7;
+        break;
+      case "2":
+        precio = 10;
+        break;
+      case "3":
+        precio = 8;
+        break;
+      case "4":
+        precio = 12;
+        break;
+      default:
+        precio = false; 
+    }
+    return precio
+  }
+  
