94f41ce51e6aae98141d0258d34d7947e4cb5b73
Ludmila <lusapiheffel@gmail.com>
2026-08-05 02:54:46 -0300

    Entrega 7

--- javascript/main.js
+++ javascript/main.js
@@ -1,45 +1,53 @@
+//Etapa 1: Inicio de sesión
+const botonIngresar = document.querySelector("#botonIngresar")
+const usuarioHtml = document.querySelector("#usuarioHtml")
+const passwordHtml = document.querySelector("#passwordHtml")
+const login = document.querySelector("#login")
+
 //iniciar sesión de usuario
-let usuarioLogueado; //nota para mí 1: se pone esto acá xq sino, cuando esto corresponda a algo, solo va a existir dentro del if y no para el resto del codigo
+let usuarioLogueado; 
 class Usuario {
- constructor(user, password, dinero, divisa) {
+ constructor(user, password, dinero, pfp) {
    this.user = user
    this.password = password
    this.dinero = dinero
+   this.pfp = pfp
  }
 }
 //Instancias (usuarios)
-    const usuario1 = new Usuario("Maguito", "1234", 30)
-    const usuario2 = new Usuario("Elfo", "5678", 45)
-    const usuario3 = new Usuario("Duende", "9000", 15)
-let usuariosExistentes = [usuario1, usuario2, usuario3];
-let usuarioIngresado = prompt("Ingresar su usuario");
-const usuario = usuariosExistentes.find(persona => persona.user === usuarioIngresado); //nota para mí: se busca en el array, NO en la class  
-if(!usuario){ //nota para mí 2: JS no puede leer la etiqueta(user) si la caja(usuario) no existe
-  alert("Ese usuario no existe")
-}else{
-  //Contraseña
-let passIngresada = prompt("Ingresar su contraseña");
-if (passIngresada === usuario.password){
-  usuarioLogueado = usuario//nota para mí 3: esto guarda la información del usuario que se logueó, osea de la caja que elejiste, no la fábrica
-  console.log("Iniciaste sesión")
-  iniciarCarrito();
+    const usuario1 = new Usuario("Maguito", "1234", 30, "../images/maguito.png")
+    const usuario2 = new Usuario("Elfo", "5678", 45, "../images/elfo.png")
+    const usuario3 = new Usuario("Duende", "9000", 15, "../images/duende.png")
+      let usuariosExistentes = [usuario1, usuario2, usuario3];
+//login
+  botonIngresar.addEventListener('click', () => {
+  let usuarioIngresado = usuarioHtml.value;
+  const usuario = usuariosExistentes.find(persona => persona.user === usuarioIngresado); 
+    if(!usuario){ 
+    alert("Ese usuario no existe")
     }else{
-        alert ("Contraseña incorrecta");
+    //Contraseña
+  let passIngresada = passwordHtml.value;
+    if (passIngresada === usuario.password){
+      usuarioLogueado = usuario
+      console.log("Iniciaste sesión")
+      login.remove()
+      mostrarTienda()
+    }else{
+      alert ("Contraseña incorrecta");
     }
-}
-//nota para mí 4: el array es el armario, la class el estante, no te confundas porfa
-//iniciar la compra
-function iniciarCarrito(){
-  let finalizarCarrito = false ; 
-  let total = 0  
-  let compra = []
+  }
+});
+//Sesión iniciada
+let compra = []
 //Productos
-  class Producto {
- constructor(nombre, id, precio, promocion) {
+class Producto {
+ constructor(nombre, id, precio, promocion, imagen) {
    this.nombre = nombre
    this.id = id
    this.precio = precio
    this.promocion = promocion
+   this.imagen = imagen
  }
  // Método para calcular si el producto tiene promoción"
  calcularPromocion() {
@@ -51,74 +59,112 @@ function iniciarCarrito(){
  }
 }
 // Instancias (productos)
-    const producto1 = new Producto("Libro Mágico", "1", 7, false)
-    const producto2 = new Producto("Poción", "2", 10, false)
-    const producto3 = new Producto("Polvo de hadas", "3", 8, true)
-    const producto4 = new Producto("Pluma de Fénix", "4", 16, false)
-    const producto5 = new Producto("Escama de dragón", "5", 29, false)
-    const producto6 = new Producto("Huevo dorado", "6", 50, false)
-    const producto7 = new Producto("Cristal encantado", "7", 8, true)
-    const producto8 = new Producto("Musgo misterioso", "8", 5, false)
+    const producto1 = new Producto("Libro Mágico", "1", 7, false, "../images/libro.png")
+    const producto2 = new Producto("Poción", "2", 10, false, "../images/pocion.png")
+    const producto3 = new Producto("Polvo de hadas", "3", 8, true, "../images/polvo.png")
+    const producto4 = new Producto("Pluma de Fénix", "4", 16, false, "../images/pluma.png")
+    const producto5 = new Producto("Escama de dragón", "5", 29, false, "../images/escama.png")
+    const producto6 = new Producto("Huevo dorado", "6", 50, false, "../images/huevo.png")
+    const producto7 = new Producto("Cristal encantado", "7", 8, true, "../images/cristal.png")
+    const producto8 = new Producto("Musgo misterioso", "8", 5, false, "../images/musgo.png")
 let productosDisponibles = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8]
-//Compra
-while (!finalizarCarrito){
-  let listaRecomendacion = ""
-        let nombreProducto = prompt("Ingrese el ID del producto que desea comprar");
-        let productoEncontrado = false;
-      //Buscar el producto
-        for (let producto of productosDisponibles){
-          if (nombreProducto === producto.id){
-        productoEncontrado = true;
-          }
-        }
-  //Para ver que productos podés comprar con el dinero disponible
-      if (nombreProducto === "Recomendame"){
-        const recomendacion = productosDisponibles.filter(producto => producto.precio <= usuarioLogueado.dinero)
-        for (let producto of recomendacion){
-                  listaRecomendacion += "\n -" + producto.nombre
-                }
-              alert("Los siguientes productos se encuentran dentro de tu presupuesto" +listaRecomendacion)
-              continue
-                }
-   //Para ver el catálogo de productos
-      if (nombreProducto === "Catálogo"){
-        const catalogo = productosDisponibles.map(producto => {return "ID: " + producto.id + " - " + producto.nombre + " - $" + producto.precio})
-          alert(catalogo.join("\n"))
-          continue
-        }
-          //Si el campo se deja vacío
-            if (nombreProducto === ""){ 
-              finalizarCarrito = true ; 
-          //Si el producto NO existe
-            } else if (productoEncontrado === false){ 
-              alert("Ingrese un producto válido"); 
-          //Si el producto existe
-            } else { 
-              for (let producto of productosDisponibles){
-                  if (nombreProducto === producto.id){
-                      compra.push (producto)
-                      console.log("Lista:", producto.nombre, "Precio:", producto.precio)
-                      total += producto.calcularPromocion ()
-                  }
-              }
-            }
+//Etapa 2: Mostrar la tienda
+function mostrarTienda(){
+//mostrar los datos del usuario
+  const perfil =document.querySelector("#perfil")
+  perfil.innerHTML =`
+  <img src="${usuarioLogueado.pfp}" alt="" class="dibujos-usuarios">
+  <h2>${usuarioLogueado.user}</h2>
+  <p>Dinero: $${usuarioLogueado.dinero}</p>
+  `
+//mostrar los productos
+  const tiendaHtml = document.querySelector("#tienda")
+  tiendaHtml.innerHTML = "";
+  productosDisponibles.forEach(producto => {
+    tiendaHtml.innerHTML += `
+      <div>
+        <img src="${producto.imagen}" alt="" class="productos" data-nombre="${producto.nombre}">
+        <h3>${producto.nombre}</h3>
+        <p>$${producto.precio}</p>
+      </div>
+    `;
+  });
+  mostrarCarrito()
+  crearBotonComprar();
+  activarEventos ()
+};
+//Activar el click de los productos
+function activarEventos(){
+  const imagenes = document.querySelectorAll(".productos");
+  imagenes.forEach(imagen => {
+    imagen.addEventListener("click", () => {
+    const producto = productosDisponibles.find(
+    p => p.nombre === imagen.dataset.nombre
+  );
+  console.log(producto.nombre);
+  compra.push(producto);
+  mostrarCarrito();
+    });
+  });
+}
+//Mostrar el carrito que vas eligiendo
+function mostrarCarrito(){
+//la pagina te muestra esa lista
+  const carritoHtml = document.querySelector ("#carrito")
+  carritoHtml.innerHTML = "";
+  compra.forEach(producto => {
+    carritoHtml.innerHTML += `
+      <img src="${producto.imagen}" alt="" class="productosSeleccionados" data-nombre="${producto.nombre}">
+    `
+  eliminarProducto()
+  })
+}
+//Eliminar productos del carrito
+function eliminarProducto (){
+  const carritoHtml = document.querySelector ("#carrito")
+  const imagenesCarrito = document.querySelectorAll(".productosSeleccionados");
+  imagenesCarrito.forEach(imagen => {
+    imagen.addEventListener("click", () => {      
+      const indice = compra.findIndex(producto => producto.nombre === imagen.dataset.nombre);
+      compra.splice(indice, 1);
+      mostrarCarrito()
+    });
+  })
+}
+//botón de la compra
+function crearBotonComprar ( ){
+  const tiendaHtml = document.querySelector("#tienda")
+  tiendaHtml.innerHTML += `<button id="botonComprar" type="button">Comprar</button>`
+  const botonComprar = document.querySelector("#botonComprar")
+  botonComprar.addEventListener("click", () => {
+    confirmarCompra()
+  });
+}
+//Cuando finalizas el carrito
+function confirmarCompra(){
+  let total = 0;
+  compra.forEach(producto => {
+    total += producto.calcularPromocion()
+  });
+  const confirmacion = document.querySelector("#confirmacion");
+    confirmacion.innerHTML = `
+      <p>¿Desea concretar la compra?</p>
+    `;
+    compra.forEach(producto => {
+      confirmacion.innerHTML += `
+        <img src="${producto.imagen}" class="productosConfirmacion">
+      `
+    });
+  confirmacion.innerHTML += `<p>Total: $${total}</p>`
+  confirmacion.innerHTML += `<button id="botonAceptar" type="button">Aceptar</button>`;
+  const botonAceptar = document.querySelector("#botonAceptar");
+  botonAceptar.addEventListener("click", () => {
+    if(total <= usuarioLogueado.dinero){
+      confirmacion.innerHTML = `
+    <p>La compra se realizó con éxito</p>`
+    }else if(total > usuarioLogueado.dinero){
+      confirmacion.innerHTML = `
+    <p>"No tiene dinero suficiente."</p>`
     }
-  //Confirmación de la compra
-    if(compra.length > 0 ){ // (Si lista no está vacía)
-       let listaCompra = ""
-          for (let producto of compra){
-            listaCompra += "\n -" + producto.nombre
-          }
-      let respuesta = confirm("Desea concretar la compra de " +listaCompra + "\n" + "El total es $" +total); //confirm solo te deja responder como true o false)
-        if(respuesta){
-          if(total === 0){ //si no se seleccionan productos
-            alert("Usted no ha comprado nada")
-             iniciarCarrito()
-          }else if(total <= usuarioLogueado.dinero){  //si alcanza el dinero
-            alert("Usted compró " +listaCompra);
-          }else if(total > usuarioLogueado.dinero){ //si no alcanza el dinero
-            alert("No tiene dinero suficiente.")
-          }
-        }
-    }
-} 
+  });
+};
